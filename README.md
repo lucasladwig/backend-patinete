1498cfe063524cb08dcb5fc2403c5fbd15760325
Lucas Ladwig <lucasladwig@gmail.com>
2023-10-23 19:05:28 -0300

    adicionando servico de cadastro de usuarios

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# backend-patinete
-Backend de um aplicativo de aluguel de patinete usando microsserviços
+# Atividade 3 - Backend
+Você deve criar um backend para um aplicativo de aluguel de patinetes elétricos. O backend será composto por cinco microservices, responsáveis pelas funcionalidades descritas a seguir:
+
+## Cadastro de Usuários
+Mantém os dados de cada usuário do sistema, como o nome, CPF, e-mail e número de telefone celular.
+
+## Cadastro de Patinetes
+Registro dos patinetes, com o número serial do equipamento, status (disponível, alugado ou fora de operação) e suas coordenadas (latitude e longitude); as coordenadas devem ser atualizadas periodicamente pelo patinete, e o usuário poderá obter uma lista com todos os patinetes disponíveis para aluguel próximos à sua localização.
+
+## Controle de Aluguéis
+Registra o tempo de início e fim dos aluguéis de patinetes; modifica o status do patinete no início e no fim do aluguel; ativa os serviços de controle de acesso ao patinete no início e no final do aluguel.
+
+## Controle de acesso ao patinete
+Bloqueia/debloqueia o patinete no início/fim de cada aluguel (basta imprimir uma mensagem na tela dizendo que o patinete foi bloqueado/desbloqueado).
+
+## Serviço de pagamento
+Registra o cartão usado para pagamento e efetua a cobrança ao final do aluguel (basta simular a cobrança e registrar no banco).
---
+++ usuario-cadastro/usuario-cadastro.js
@@ -0,0 +1,131 @@
+// Inicia o Express.js
+const express = require("express");
+const app = express();
+
+// Body Parser - usado para processar dados da requisição HTTP
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Inicia o Servidor na porta 8080
+let porta = 8080;
+app.listen(porta, () => {
+  console.log("Servidor em execução na porta: " + porta);
+});
+
+// Importa o package do SQLite
+const sqlite3 = require("sqlite3");
+
+// Acessa o arquivo com o banco de dados
+var db = new sqlite3.Database("./dados-usuario.db", (err) => {
+  if (err) {
+    console.log("ERRO: não foi possível conectar ao SQLite.");
+    throw err;
+  }
+  console.log("Conectado ao SQLite!");
+});
+
+// MÉTODOS CRUD HTTP
+// Cria a tabela 'usuario', caso ela não exista
+db.run(
+  `CREATE TABLE IF NOT EXISTS usuario 
+        (cpf INTEGER PRIMARY KEY NOT NULL UNIQUE,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL, 
+            telefone INTEGER NOT NULL)`,
+  [],
+  (err) => {
+    if (err) {
+      console.log("ERRO: não foi possível criar tabela.");
+      throw err;
+    }
+  }
+);
+
+// POST /usuario - CADASTRAR um novo usuário
+app.post("/usuario", (req, res, next) => {
+  db.run(
+    `INSERT INTO usuario(cpf, nome, email, telefone) VALUES(?, ?, ?, ?)`,
+    [req.body.cpf, req.body.nome, req.body.email, req.body.telefone],
+    (err) => {
+      if (err) {
+        console.log("Error: " + err);
+        res.status(500).send("Erro ao cadastrar usuário.");
+      } else {
+        console.log("Usuário cadastrado com sucesso!");
+        res.status(200).send("Usuário cadastrado com sucesso!");
+      }
+    }
+  );
+});
+
+// GET /usuario - RETORNAR todos os usuários
+app.get("/usuario", (req, res, next) => {
+  db.all(`SELECT * FROM usuario`, [], (err, result) => {
+    if (err) {
+      console.log("Erro: " + err);
+      res.status(500).send("Erro ao obter dados de usuários.");
+    } else if (result.length === 0) {
+        console.log("Lista de usuários vazia!");
+        res.status(500).send("Lista de usuários vazia!");
+
+    } else {
+      res.status(200).json(result);
+    }
+  });
+});
+
+// GET /usuario/:cpf - RETORNAR usuário com base no CPF
+app.get("/usuario/:cpf", (req, res, next) => {
+  db.get(
+    `SELECT * FROM usuario WHERE cpf = ?`,
+    req.params.cpf,
+    (err, result) => {
+      if (err) {
+        console.log("Erro: " + err);
+        res.status(500).send("Erro ao obter dados de usuários.");
+      } else if (result == null) {
+        console.log("Usuário não encontrado.");
+        res.status(404).send("Usuário não encontrado.");
+      } else {
+        res.status(200).json(result);
+      }
+    }
+  );
+});
+
+// PATCH /usuario/:cpf - ALTERAR o cadastro de um usuário
+app.patch("/usuario/:cpf", (req, res, next) => {
+  db.run(
+    `UPDATE usuario 
+        SET nome = COALESCE(?, nome), 
+        email = COALESCE(?, email),
+        telefone = COALESCE(?, telefone)
+        WHERE cpf = ?`,
+    [req.body.nome, req.body.email, req.body.telefone, req.params.cpf],
+    function (err) {
+      if (err) {
+        res.status(500).send("Erro ao alterar dados.");
+      } else if (this.changes == 0) {
+        console.log("Usuário não encontrado.");
+        res.status(404).send("Usuário não encontrado.");
+      } else {
+        res.status(200).send("Usuário alterado com sucesso!");
+      }
+    }
+  );
+});
+
+// DELETE /usuario/:cpf - REMOVER um usuário do cadastro
+app.delete("/usuario/:cpf", (req, res, next) => {
+  db.run(`DELETE FROM cadastro WHERE cpf = ?`, req.params.cpf, function (err) {
+    if (err) {
+      res.status(500).send("Erro ao remover usuário.");
+    } else if (this.changes == 0) {
+      console.log("Usuário não encontrado.");
+      res.status(404).send("Usuário não encontrado.");
+    } else {
+      res.status(200).send("Usuário removido com sucesso!");
+    }
+  });
+});
